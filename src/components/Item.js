df6573c4882e28c78fb3229ac44e0143974b213f
alexsylvian <lazeratus1@gmail.com>
2023-10-22 18:59:09 -0400

    state test passed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isAdded, setIsAdded] = useState(false)
+
+  const buttonClass = isAdded ? "in-cart" : "add"
+  const buttonText = isAdded ? "Remove from Cart" : "Add to Cart";
+
+
+  function handleClick(){
+    setIsAdded(!isAdded)
+  }
+
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleClick} className={buttonClass}>{buttonText}</button>
     </li>
   );
 }
